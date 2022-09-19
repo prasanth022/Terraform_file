0268db29b583c3777045b29fbacb6fd4398d7170
prasanth022 <92729994+prasanth022@users.noreply.github.com>
2022-09-19 21:38:21 +0530

    Update vpc_ec2.tf

--- vpc_ec2.tf
+++ vpc_ec2.tf
@@ -1,24 +1,24 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "tf-example"
+    Name = "my_vpc_latest"
   }
 }
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-south-1a"
 
   tags = {
-    Name = "tf-example"
+    Name = "My_subnet_latest"
   }
 }
 
 resource "aws_network_interface" "foo" {
   subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+  private_ips = ["10.0.1.100"]
 
   tags = {
     Name = "primary_network_interface"
@@ -26,7 +26,7 @@ resource "aws_network_interface" "foo" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami   = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
 
   network_interface {
