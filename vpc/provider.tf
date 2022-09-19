dde5230d79a8b79b2b7f135554b1c529fcd2e98c
prasanth022 <92729994+prasanth022@users.noreply.github.com>
2022-09-19 22:27:26 +0530

    Update provider.tf

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,5 +1,5 @@
  provider "aws" { 
      region = "us-east-1" 
-     access_key = "AKIAXB5XR7GH4GDEJ77X" 
-     secret_key = "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"
+     access_key = "" 
+     secret_key = ""
      }
